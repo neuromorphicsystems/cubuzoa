fe51327c89e9d3d465ee3b19f9cb9d8dcbdc1d69
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2021-05-09 17:29:02 +0200

    Add a missing sudo for Linux

--- backend/setuptools.build_meta/linux.py
+++ backend/setuptools.build_meta/linux.py
@@ -1,6 +1,6 @@
 def os_build(common, versions, project, wheels, build):
     common.info(f'Copying project files to Linux')
-    common.vagrant_run(build, 'rm -rf project; sudo rm -rf wheels')
+    common.vagrant_run(build, 'sudo rm -rf project; sudo rm -rf wheels')
     common.rsync(build, host_path=project, guest_path='project', host_to_guest=True)
     for version in versions:
         common.info(f'Building for Python {version} on Linux')
