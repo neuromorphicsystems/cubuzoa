2157ded87acd20659facea5a4713158559d0a1c9
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2021-05-09 18:20:29 +0200

    Audit linux wheels

--- backend/setuptools.build_meta/linux.py
+++ backend/setuptools.build_meta/linux.py
@@ -4,7 +4,12 @@ def os_build(common, versions, project, wheels, build):
     common.rsync(build, host_path=project, guest_path='project', host_to_guest=True)
     for version in versions:
         common.info(f'Building for Python {version} on Linux')
-        common.linux_docker_run(build, '{} {}'.format(
-            common.os_to_configuration['linux'].version_to_name[version],
-            common.pip_wheel('/wheels')))
+        common.linux_docker_run(build, ' && '.join((
+            'mkdir /unaudited-wheels',
+            '{} {}'.format(
+                common.os_to_configuration['linux'].version_to_name[version],
+                common.pip_wheel('/unaudited-wheels'),
+            ),
+            'for wheel_file in /unaudited-wheels/*.whl; do auditwheel repair $wheel_file -w /wheels; done',
+        )))
         common.rsync(build, host_path=wheels, guest_path='wheels/', host_to_guest=False)
